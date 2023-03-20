637b365054fc65e07731194834766f81c23b484a
Chikaodili Deng <mchikky@yahoo.com>
2023-03-20 15:50:35 +0100

    Initial Commit

---
+++ NavigationList/NavigationListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationListApp.swift
+//  NavigationList
+//
+//  Created by Chikaodili Deng on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
