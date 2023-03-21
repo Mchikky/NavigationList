f6a3a5c051f44055a080a43ea769b18e90780a7a
Chikaodili Deng <mchikky@yahoo.com>
2023-03-21 16:38:30 +0100

    Navigation list

---
+++ NavigationList/AnimalDetail.swift
@@ -0,0 +1,20 @@
+//
+//  AnimalDetail.swift
+//  NavigationList
+//
+//  Created by Chikaodili Deng on 21/03/2023.
+//
+
+import SwiftUI
+
+struct AnimalDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimalDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimalDetail()
+    }
+}
--- NavigationList/ContentView.swift
+++ NavigationList/ContentView.swift
@@ -9,16 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(listAnimals) { animals in
+                NavigationLink {
+                    AnimalDetail()
+                } label: {
+                    HStack {
+                        Image(animals.image)
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
+                        Text(animals.name)
+                    }
+                }
+                .navigationTitle("Animal")
+                    }
+                }
+            }
+          
         }
-        .padding()
-    }
-}
+    
 
+var listAnimals = [
+    Animal(name:"Deer", image:"Deer",country: "England" ),
+    Animal(name: "Bear", image:"Bear", country:"Scotland"),
+    Animal(name:"Bull", image:"Bull", country: "Mexico"),
+    Animal(name: "Duck", image: "Duck", country: "France"),
+    Animal(name: "Eagle", image: "Eagle", country: "United States"),
+    Animal(name: "Fox", image: "Fox", country: "Antarctica"),
+    Animal(name: "Horse",image: "Horse", country: "Brazil"),
+    Animal(name: "Monkey", image:"Monkey", country: "Indonesia")
+]
+
+
+struct Animal: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var country: String
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
